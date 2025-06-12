4282798bd172685ac361dd521c44638e79fc89a3
github_cloudlabsuser_1216 <github_cloudlabsuser_1216@cloudlabsaiuser.com>
2025-06-12 14:11:51 +0000

    Added vulnerability

--- src/ContosoTraders.Ui.Website/src/App.js
+++ src/ContosoTraders.Ui.Website/src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import { connect } from "react-redux";
 // import { CartService } from "./services";
 // import Meeting from './pages/home/components/videoCall/Meeting';
-
 import Header from "./components/header/header";
 import HeaderMessage from "./components/header/headerMessage";
 import Appbar from "./components/header/appbar";
@@ -23,20 +22,23 @@ import {
   ErrorPage,
   Cart,
 } from "./pages";
-
 // import "./i18n";
 import "./main.scss";
 import warningIcon from './assets/images/original/Contoso_Assets/Icons/information_icon.svg'
 import { useLocation } from "react-router-dom";
 import { CartService } from "./services";
 import { getCartQuantity } from "./actions/actions";
-
-
   function App(props) {
     const location = useLocation()
-    // const [shoppingCart, setShoppingCart] = useState([])
     const [quantity, setQuantity] = useState(0)
-
+    // Add this test vulnerability - Unsafe DOM manipulation
+    useEffect(() => {
+      // WARNING: This is intentionally vulnerable code for testing CodeQL
+      const userInput = new URLSearchParams(location.search).get('message');
+      if (userInput) {
+        document.getElementById('box').innerHTML = userInput; // Unsafe direct DOM manipulation
+      }
+    }, [location]);
     const getQuantity = useCallback(async() => {
       let quantity = 0;
       //Show cart using API
@@ -54,19 +56,15 @@ import { getCartQuantity } from "./actions/actions";
       }
       setQuantity(quantity);
     },[props])
-    
     useEffect(() => {
       props.getCartQuantity(quantity)
     }, [quantity, props]);
-
     React.useEffect(() => {
       getQuantity()
     }, [getQuantity]);
-
     React.useEffect(() => {
         window.scrollTo(0, 0);
     }, [location.pathname]);
-
     return (
       <div className={`App ${props.theme ? 'dark' : 'light' }`}>
         <Fragment>
@@ -115,9 +113,8 @@ import { getCartQuantity } from "./actions/actions";
     );
   }
 // }
-
-const mapStateToProps = (state) => { 
-  return { 
+const mapStateToProps = (state) => {
+  return {
     userInfo : state.login.userInfo,
     theme :  state.login.theme
   }
